3baca621b5c927e9603be847e0d688f189245acd
sandeshgajare55 <sandeshgajare35@gmail.com>
2023-09-19 18:39:13 +0530

    Minor Changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
         window.scrollTo(0,0);
       }}>
       <img src={logo} alt='logo' className='w-9 h-9 object-contain'/>
-      <p className='text-white text-[18px] font-bold cursor-pointer flex'>Sandesh&nbsp;<span className=' sm:block hidden'>| JavaScript Mastery</span> </p>
+      <p className='text-white text-[18px] font-bold cursor-pointer flex'>Sandesh&nbsp;<span className=' sm:block hidden'>| Dynamic Site</span> </p>
       </Link>
       <ul className='list-none hidden sm:flex flex-row gap-10'>
         {navLinks.map((Link)=>(
