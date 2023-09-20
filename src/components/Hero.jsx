fc379baf6de9c1060671f8525ea1b651652b333d
sandeshgajare55 <sandeshgajare35@gmail.com>
2023-09-21 00:01:36 +0530

    Added Computer About Section as well as Hero Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className={`relative w-full h-screen mx-auto`}>
+      <div
+        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+      >
+        <div className='flex flex-col justify-center items-center mt-5'>
+          <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
+          <div className='w-1 sm:h-80 h-40 violet-gradient' />
+        </div>
 
-export default Hero
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I'm <span className='text-[#915EFF]'>Sandesh Gajare</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            A passionate and results-driven software engineer with a strong
+            background in Java, Python, SQL, and good in the Django framework.<br/>
+            I bring over 1.2 years of experience working with cutting-edge
+            technology in the software industry.Also a dedicated professional
+            with a passion for seamless implementation and exceptional customer
+            support.<br/>I've had the privilege of contributing my skills and
+            knowledge to two esteemed product-based companies, where I played a
+            pivotal role in developing innovative solutions and driving product
+            excellence.<br/>My time at these companies has equipped me with a deep
+            understanding of software development, from concept to deployment.
+          </p>
+        </div>
+      </div>
+
+      <ComputersCanvas />
+
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+        <a href='#about'>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+            <motion.div
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className='w-3 h-3 rounded-full bg-secondary mb-1'
+            />
+          </div>
+        </a>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
